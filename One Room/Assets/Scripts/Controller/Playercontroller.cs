94b46919a29219a322bc2cc782b52aa8ab6e051b
Pun Jelly <heroswift15@gmail.com>
2022-03-01 08:30:22 +0900

    마우스로 화면 제어
    마우스로 화면 제어

--- One Room/Assets/Scripts/Controller/Playercontroller.cs
+++ One Room/Assets/Scripts/Controller/Playercontroller.cs
@@ -6,10 +6,133 @@ public class Playercontroller : MonoBehaviour
 {
     [SerializeField] Transform TF_crosshair;
 
+    [SerializeField] Transform TF_Cam;
+
+    [SerializeField] Vector2 CamBoundary; // 캠의 가두기 영역
+
+    [SerializeField] float sightMoveSpeed; // 좌우 움직임 스피드
+
+    [SerializeField] float sightSensitivity; // 고개움직임 속도
+
+    [SerializeField] float lookLimit_X;
+    [SerializeField] float lookLimit_Y;
+
+    float CurrentAngle_X;
+    float CurrentAngle_Y;
+
     void Update()
     {
         CrosshairMoving();
+        ViewMoving();
+        KeyViewMoving();
+        CameraLimit();
     }
+
+    void CameraLimit()
+    {
+        if(TF_Cam.localPosition.x >= CamBoundary.x)
+        {
+            TF_Cam.localPosition = new Vector3(CamBoundary.x
+                                              ,TF_Cam.localPosition.y
+                                              ,TF_Cam.localPosition.z);
+        }
+
+        else if(TF_Cam.localPosition.x <= -CamBoundary.x)
+        {
+            TF_Cam.localPosition = new Vector3(-CamBoundary.x
+                                              ,TF_Cam.localPosition.y
+                                              ,TF_Cam.localPosition.z);
+        }
+
+        
+        if(TF_Cam.localPosition.y >= CamBoundary.y+1)
+        {
+            TF_Cam.localPosition = new Vector3(TF_Cam.localPosition.x
+                                        ,CamBoundary.y+1
+                                        ,TF_Cam.localPosition.z);
+
+        }
+
+        else if(TF_Cam.localPosition.y <= 1-CamBoundary.y)
+        {
+            TF_Cam.localPosition = new Vector3(TF_Cam.localPosition.x
+                                        ,1-CamBoundary.y
+                                        ,TF_Cam.localPosition.z);
+
+        }
+
+
+    }
+
+    void KeyViewMoving()
+    {
+        if(Input.GetAxisRaw("Horizontal") !=0)
+        {
+            CurrentAngle_Y += sightSensitivity *Input.GetAxisRaw("Horizontal");
+
+            CurrentAngle_Y = Mathf.Clamp(CurrentAngle_Y,-lookLimit_X ,lookLimit_X);
+
+            TF_Cam.localPosition =
+             new Vector3(TF_Cam.localPosition.x + sightMoveSpeed*Input.GetAxisRaw("Horizontal")
+                        ,TF_Cam.localPosition.y
+                        ,TF_Cam.localPosition.z);
+        }
+
+        if(Input.GetAxisRaw("Vertical") !=0)
+        {
+            CurrentAngle_X += sightSensitivity * -Input.GetAxisRaw("Vertical");
+
+            CurrentAngle_X = Mathf.Clamp(CurrentAngle_X,-lookLimit_Y ,lookLimit_Y);
+
+            TF_Cam.localPosition =
+             new Vector3(TF_Cam.localPosition.x 
+                        ,TF_Cam.localPosition.y + sightMoveSpeed*Input.GetAxisRaw("Vertical")
+                        ,TF_Cam.localPosition.z);
+        }
+
+        TF_Cam.localEulerAngles = new Vector3(CurrentAngle_X,CurrentAngle_Y,TF_Cam.localEulerAngles.z);
+
+    }
+
+    void ViewMoving()
+    {
+        if(TF_crosshair.localPosition.x > (Screen.width/2-100) 
+        || TF_crosshair.localPosition.x < (-Screen.width/2+100))
+        {
+            CurrentAngle_Y += (TF_crosshair.localPosition.x > 0) ? sightSensitivity : -sightSensitivity;
+
+            CurrentAngle_Y = Mathf.Clamp(CurrentAngle_Y,-lookLimit_X,lookLimit_X);
+
+
+            float t_applyspeed = (TF_crosshair.localPosition.x >0) ? sightMoveSpeed : -sightMoveSpeed;
+            TF_Cam.localPosition = new Vector3(TF_Cam.localPosition.x+t_applyspeed,
+                                               TF_Cam.localPosition.y,TF_Cam.localPosition.z);
+
+        }
+
+
+        if(TF_crosshair.localPosition.y > (Screen.height/2-100) 
+        || TF_crosshair.localPosition.y < (-Screen.height/2+100))
+        {
+            CurrentAngle_X += (TF_crosshair.localPosition.y > 0) ? -sightSensitivity : sightSensitivity;
+
+            CurrentAngle_X = Mathf.Clamp(CurrentAngle_X,-lookLimit_Y,lookLimit_Y);
+
+
+            float t_applyspeed = (TF_crosshair.localPosition.y >0) ? sightMoveSpeed : -sightMoveSpeed;
+            TF_Cam.localPosition = new Vector3(TF_Cam.localPosition.x,
+                                               TF_Cam.localPosition.y+t_applyspeed,TF_Cam.localPosition.z);
+
+
+        }
+
+
+
+
+        TF_Cam.localEulerAngles = new Vector3(CurrentAngle_X,CurrentAngle_Y,TF_Cam.localEulerAngles.z);
+
+    }
+
 
     void CrosshairMoving()
     {
